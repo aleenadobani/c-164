f8ea383a03e216b4e633dd9c7fc71b7a5fe70688
aleenadobani <98019373+aleenadobani@users.noreply.github.com>
2023-06-15 10:55:56 -0400

    Add files via upload

---
+++ c-164.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 13:20:23 2023
+
+@author: User
+"""
+
+from tkinter import *
+
+root = Tk()
+root.title("planet Encyclopedia")
+root.geometry("500x500")
+root.configure(background = "lightblue")
+
+label_planet = Label(root, text ="planet :", bg="lightblue")
+label_planet_name = Label(root,font=("courier",18,"bold"),bg="lightblue")
+label_planet_image = Label(root,bg="gold2", highlightthickness=5, borderwidth=2,relief=SOLID)
+label_planet_gravity_radius = Label(root,font=("castellar"), bg="lightblue")
+label_planet_info = Label(root,font=("courier",10,"bold"), bg="lightblue",wraplength=500)
+            
+def planetInfo():
+    print("hi")
+    
+btn = Button(root, text="show planet Info", command=planetInfo)
+btn.place(relx=0.5, rely=0.18, anchor=CENTER)
+
+label_planet.place(relx=0.2, rely=0.1, anchor=CENTER)
+label_planet_name.place(relx=0.5,rely=0.25, anchor=CENTER)
+label_planet_image.place(relx=0.5,rely=0.5, anchor=CENTER)
+label_planet_gravity_radius.place(relx=0.5,rely=0.8, anchor=CENTER)
+label_planet_info.place(relx=0.5,rely=0.9, anchor=CENTER)
+
+root.mainloop()
